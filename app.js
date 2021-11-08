dfaf09bffac30d3efc4760672fd57a2f73d20788
T3rra34 <T3rra34@github.com>
2021-11-08 15:39:45 +0200

    Done city data fetching

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-function weatherDataFetch(city) {
-	var key = '9e2145bbf1391d096fb2e91365d0313c';
+document.addEventListener('DOMContentLoaded', cityWeather)
+
+
+function weatherDataFetch(	city  ) {
+	var key = '5cc2e697de6224a254700ae41a4e95d1';
 	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
 	.then(function(resp) {
 		return resp.json()
 	})
 	.then(function(data) {
 		console.log(data);
+		drawWeather(data);
 	})
 	.catch(function() {
 	});
 }
 
 function cityWeather(e) {
-	weatherDataFetch('Tallinn');
+	weatherDataFetch( 'Tallinn' );
+}
+
+function drawWeather(e) {
+	var celcius = Math.round(parseFloat(data.main.temp)-273.15);
+	var description = data.weather[0].description;
+
+	document.querySelector('#description').innerHTML = description;
+	document.querySelector('#temp').innerHTML = celcius + '&deg;' ;
+	document.querySelector('#location').innerHTML = data.name;
 }
